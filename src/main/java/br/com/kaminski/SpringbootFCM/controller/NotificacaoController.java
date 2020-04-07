dee766d653e8f57650c8e5edf6cfa13e0b388090
willian-kaminski <developer.williansantos@gmail.com>
2020-04-07 00:05:13 -0300

    Ajustando o mapeamento dos endpoints.

--- src/main/java/br/com/kaminski/SpringbootFCM/controller/NotificacaoController.java
+++ src/main/java/br/com/kaminski/SpringbootFCM/controller/NotificacaoController.java
@@ -5,21 +5,18 @@ import br.com.kaminski.SpringbootFCM.service.NotificacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("notificar")
+@RequestMapping("/notificar")
 public class NotificacaoController {
 
     @Autowired
     private NotificacaoService notificacaoService;
 
     @PostMapping
-    @RequestMapping("topico")
-    public ResponseEntity<Object> gerarNotificacaoPorTopico(@RequestParam NotificacaoTopicoForm notificacaoTopicoForm){
+    @RequestMapping("/topico")
+    public ResponseEntity<Object> gerarNotificacaoPorTopico(@RequestBody NotificacaoTopicoForm notificacaoTopicoForm){
         notificacaoService.enviarNotificacaoPorTopico(notificacaoTopicoForm);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
