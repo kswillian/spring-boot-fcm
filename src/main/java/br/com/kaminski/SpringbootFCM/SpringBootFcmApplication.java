9dbb48aa3ff858d2eb79a913bf99a1b9462a0a23
willian-kaminski <developer.williansantos@gmail.com>
2020-04-06 22:34:56 -0300

    adding project

---
+++ src/main/java/br/com/kaminski/SpringbootFCM/SpringBootFcmApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaminski.SpringbootFCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFcmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFcmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kaminski/SpringbootFCM/SpringBootFcmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaminski.SpringbootFCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
